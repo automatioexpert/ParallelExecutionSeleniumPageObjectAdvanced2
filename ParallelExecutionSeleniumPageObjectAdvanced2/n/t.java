2255f6624063eb889c76af1a4c53fbf89737a1d3
Valmiki Vishwakarma <you@example.com>
2022-12-10 00:07:17 +0530

    adding file.

---
+++ ParallelExecutionSeleniumPageObjectAdvanced2/n/t.java
@@ -0,0 +1,9 @@
+
+public class t {
+
+	public static void main(String[] args) {
+	System.out.println("new src folder");
+
+	}
+
+}
---
+++ ParallelExecutionSeleniumPageObjectAdvanced2/src2/test/login.java
@@ -0,0 +1,8 @@
+package test;
+
+public class login {
+	public static void main(String[] args) {
+		System.out.println("hi");
+	}
+
+}
